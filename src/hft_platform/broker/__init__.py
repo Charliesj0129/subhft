16b8cfaa7188a351d5ac6fd0c3d81ceed4df1520
Charliesj0129 <charlie.sj502@gmail.com>
2026-03-11 10:46:55 +0800

    feat: add multi-broker config schema and per-broker YAML configs

---
+++ src/hft_platform/broker/__init__.py
@@ -0,0 +1,19 @@
+from .config import (
+    BrokerAuthConfig,
+    BrokerCapabilitiesConfig,
+    BrokerConfig,
+    BrokerLatencyProfile,
+    BrokerRateLimits,
+    BrokerTransportConfig,
+    load_broker_config,
+)
+
+__all__ = [
+    "BrokerAuthConfig",
+    "BrokerCapabilitiesConfig",
+    "BrokerConfig",
+    "BrokerLatencyProfile",
+    "BrokerRateLimits",
+    "BrokerTransportConfig",
+    "load_broker_config",
+]
