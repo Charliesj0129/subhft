435817b11b235f46634eb5c3484570f7d3c85f3b
Charliesj0129 <charlie.sj502@gmail.com>
2026-04-09 23:57:33 +0800

    chore: format startup_recon.py

--- src/hft_platform/execution/startup_recon.py
+++ src/hft_platform/execution/startup_recon.py
@@ -242,9 +242,7 @@ class StartupPositionVerifier:
         from hft_platform.execution.checkpoint import PositionCheckpointWriter
 
         if trading_date is None:
-            trading_date = datetime.fromtimestamp(
-                timebase.now_s(), tz=ZoneInfo("Asia/Taipei")
-            ).strftime("%Y%m%d")
+            trading_date = datetime.fromtimestamp(timebase.now_s(), tz=ZoneInfo("Asia/Taipei")).strftime("%Y%m%d")
 
         logger.info("position_recovery: starting", trading_date=trading_date)
 
